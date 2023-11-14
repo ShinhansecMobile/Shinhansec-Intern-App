8a0be6812daa98b7b670935c3521dc288b4b221d
C0012S <alwaysh1002@gmail.com>
2023-11-15 00:06:19 +0900

    [REFACTOR] Widget 분리

---
+++ lib/screens/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to the Home Screen!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
