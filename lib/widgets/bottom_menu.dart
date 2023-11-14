c6eb16f7954ce64b3e7c08b58d12a07ee12960eb
C0012S <alwaysh1002@gmail.com>
2023-11-15 04:30:50 +0900

    [DESIGN] 하단 메뉴 디자인 수정

--- lib/widgets/bottom_menu.dart
+++ lib/widgets/bottom_menu.dart
@@ -14,66 +14,41 @@ class _BottomMenuState extends State<BottomMenu> {
 
   final List<Widget> pages = [
     Container(
-      color: Colors.red,
+      color: const Color(0xFFE5E5FF),
       alignment: Alignment.center,
-      child: const Text('Page 1'),
+      child: const Text('자산 페이지'),
     ),
     Container(
-      color: Colors.blue,
+      color: const Color(0xFFE5E5FF),
       alignment: Alignment.center,
-      child: const Text('Page 2'),
+      child: const Text('혜택 페이지'),
     ),
     const Home(),
-    // Container(
-    //   color: Colors.orange,
-    //   alignment: Alignment.center,
-    //   child: const Text('Page 4'),
-    // ),
-    // Container(
-    //   color: Colors.cyan,
-    //   alignment: Alignment.center,
-    //   child: const Text('Page 5'),
-    // ),
-    // 다른 페이지들도 추가
+    Container(
+      color: const Color(0xFFE5E5FF),
+      alignment: Alignment.center,
+      child: const Text('주식 페이지'),
+    ),
+    Container(
+      color: const Color(0xFFE5E5FF),
+      alignment: Alignment.center,
+      child: const Text('전체 메뉴 페이지'),
+    ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: pages[currentPageIndex],
-      // bottomNavigationBar: NavigationBar(
-      //   onDestinationSelected: (int index) {
-      //     setState(() {
-      //       currentPageIndex = index;
-      //     });
-      //   },
-      //   indicatorColor: Colors.amber[800],
-      //   selectedIndex: currentPageIndex,
-      //   destinations: const <Widget>[
-      //     NavigationDestination(
-      //       selectedIcon: Icon(Icons.home),
-      //       icon: Icon(Icons.home_outlined),
-      //       label: 'Home',
-      //     ),
-      //     NavigationDestination(
-      //       icon: Icon(Icons.business),
-      //       label: 'Business',
-      //     ),
-      //     NavigationDestination(
-      //       selectedIcon: Icon(Icons.school),
-      //       icon: Icon(Icons.school_outlined),
-      //       label: 'School',
-      //     ),
-      //   ],
-      // ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet), label: '자산'),
-          BottomNavigationBarItem(icon: Icon(Icons.business), label: '혜택'),
+          BottomNavigationBarItem(icon: Icon(Icons.card_giftcard), label: '혜택'),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-          // BottomNavigationBarItem(icon: Icon(Icons.trending_up), label: '주식'),
-          // BottomNavigationBarItem(icon: Icon(Icons.menu), label: '전체'),
-          // 다른 항목들도 추가
+          BottomNavigationBarItem(icon: Icon(Icons.trending_up), label: '주식'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: '전체'),
         ],
         currentIndex: currentPageIndex,
         onTap: (index) {
@@ -81,6 +56,10 @@ class _BottomMenuState extends State<BottomMenu> {
             currentPageIndex = index;
           });
         },
+        unselectedItemColor: const Color(0xFFC7C8C7), // 선택되지 않은 항목의 색상 지정
+        unselectedFontSize: 12.0, // 선택되지 않은 항목의 폰트 크기
+        selectedItemColor: const Color(0xFF8863E4), // 선택된 항목의 색상 변경
+        selectedFontSize: 12.0, // 선택된 항목의 폰트 크기
       ),
     );
   }
